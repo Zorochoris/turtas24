34f057e31bf012acef190b5e529f8c6fdd187567
Marius <marius.pikciunas@gmail.com>
2019-01-29 14:09:20 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.send("home");
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
